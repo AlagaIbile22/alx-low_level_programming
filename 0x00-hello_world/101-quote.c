7b661f240434ab0a4237d720c1078e0ad75fa208
AlagaIbile22 <shexkywizzy2011@gmail.com>
2022-09-08 10:59:49 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 
 /**
  *main - Entry point
- *return: Always 1
+ *Return: Always 1
  *
  */
 
 int main(void)
 {
-write(2,  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
