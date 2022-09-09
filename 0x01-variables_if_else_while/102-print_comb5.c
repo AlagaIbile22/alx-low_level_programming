7c9a0e14b38271afec46400dd568701b28719c68
AlagaIbile22 <shexkywizzy2011@gmail.com>
2022-09-09 13:17:23 -0700

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -26,7 +26,8 @@ int main(void)
 					putchar(t);
 					putchar(o);
 
-					if (!((tens == '9' && ones == '8') && (t == '9' && o == '9')))
+					if (!((tens == '9' && ones == '8') && (t == '9' && o == '8')))
+						continue;
 					{
 						putchar(',');
 						putchar(' ');
