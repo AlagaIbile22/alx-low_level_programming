b7cef97f7eff551529a60b4104f2b35fab6262bd
AlagaIbile22 <shexkywizzy2011@gmail.com>
2022-09-09 12:28:52 -0700

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,9 +16,9 @@ int main(void)
 	{
 		for (ones = '0'; ones <= '9'; ones++)
 		{
-			for (t = tens; t <= '9'; t++) /*print second of pair*/
+			for (t = tens + 1; t <= '9'; t++) /*print second of pair*/
 			{
-				for (o = ones; o <= '9'; o++)
+				for (o = ones + 1; o <= '9'; o++)
 				{
 					putchar(tens);
 					putchar(ones);
