a8c865ffc33dee589eb27757c3fdcf53096f4f37
AlagaIbile22 <shexkywizzy2011@gmail.com>
2022-09-13 07:03:37 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "putchar.h"
+
+/**
+ * main - print Putchar, followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('P');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
