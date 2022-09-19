4d0139973a77398cc319fe426ad8a66ab589f6e4
AlagaIbile22 <shexkywizzy2011@gmail.com>
2022-09-19 16:10:26 -0700

    reset to 98

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * Header file containing prototypes
+ */
+
+#include <stdlib.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
