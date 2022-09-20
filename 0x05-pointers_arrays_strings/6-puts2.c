45578ddbaa896fe7ad73d9cf4457ed527f456af9
AlagaIbile22 <shexkywizzy2011@gmail.com>
2022-09-19 17:06:24 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one char out of 2
+ * @str: string to print tha chars from
+ */
+
+void puts2(char *str)
+{
+	int len, i;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i <= len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
