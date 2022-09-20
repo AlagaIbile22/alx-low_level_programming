be66049b217776ce4a3a7bbbd7d38851f96d655f
AlagaIbile22 <shexkywizzy2011@gmail.com>
2022-09-19 18:40:09 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,50 +4,31 @@
 
 /**
  * main - generates keygen
- * Return: 0 always
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	char password[84];
-	int index = 0, sum = 0, diff_half1, diff_half2;
+	int pass[100];
+	int i, sum, n;
 
-	srand(time(0));
+	sum = 0;
 
-	while (sum < 2772)
+	srand(time(NULL));
+
+	for (i = 0, i < 100; i++)
 	{
-		password[index] = 33 + rand() % 94;
-		sum += password[index++];
-	}
-
-	password[index] = '\0';
-
-	if (sum != 2772)
-	{
-		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum - 2772) / 2;
-		if ((sum - 2772) % 2 != 0)
-			diff_half1++;
-
-		for (index = 0; password[index]; index++)
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
 		{
-			if (password[index] >= (33 + diff_half1))
-			{
-				password[index] -= diff_half1;
-				break;
-			}
-		}
-		for (index = 0; pasword[index]; index++)
-		{
-			if (password[index] >= (33 + diff_half2))
-			{
-				password[index] -= diff_half2;
-				break;
-			}
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
 		}
 	}
-
-	printf("%s", password);
 
 	return (0);
 }
