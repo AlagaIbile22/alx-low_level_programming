0203f27ad68383f39586d54e9e0d131eb149e763
AlagaIbile22 <shexkywizzy2011@gmail.com>
2022-09-26 12:42:02 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,27 +4,32 @@
  * _strspn - Entry point
  * @s: input
  * @accept: input
- * Return: Always 0 (Success)
+ * Return: the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int n = 0;
-	int r;
+	int i, j, f, flag;
 
-	while (*s)
+	f = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (r = 0; accept[r]; r++)
+		flag = 0;
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (*s == accepr[r])
+			if (s[i] == accept[j])
 			{
-				n++;
-				break;
+				f++;
+				flag = 1;
 			}
-			else if (accept[r + 1] == '\0')
-				return (n);
 		}
-		s++;
+		if (flag == 0)
+		{
+			return (f);
+		}
 	}
+
 	return (0);
 }
