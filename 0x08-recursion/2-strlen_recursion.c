4254d8b241fdba4475c9856d8ba17c1b1a762e1b
AlagaIbile22 <shexkywizzy2011@gmail.com>
2022-09-28 01:55:59 -0700

    2 strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string to be measured
+ * Return: the length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int longit = 0;
+
+	if (*s)
+	{
+		longit++;
+		longit += strlen_recursion(s + 1);
+	}
+
+	return (longit);
+}
