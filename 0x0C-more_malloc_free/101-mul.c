3dbeca363c643eca9a41acbc7e1ee575986f1bfe
AlagaIbile22 <shexkywizzy2011@gmail.com>
2022-10-06 18:51:05 -0700

    101 mul c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -71,7 +71,7 @@ int main(int argc, char *argv[])
 		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		digit1 = s1[1en1] - '0';
+		digit1 = s1[len1] - '0';
 		carry = 0;
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
