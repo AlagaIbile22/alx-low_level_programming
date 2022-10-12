4c263c450bbe4c1c280edd302201cef1494003b4
AlagaIbile22 <shexkywizzy2011@gmail.com>
2022-10-12 16:54:01 -0700

    op functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - returns the sum of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - returns the difference of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - returns the product of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - returns the division of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the quotient of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - returns the remainder of the division of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
